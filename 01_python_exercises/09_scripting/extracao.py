29e66063fd8b6fa54409c70927859e1289733255
gabriel-olegas <gabriel.oligar@gmail.com>
2022-08-16 23:40:17 -0300

    feat: :sparkles: add new exercise

--- 01_python_exercises/09_scripting/extracao.py
+++ 01_python_exercises/09_scripting/extracao.py
@@ -33,14 +33,14 @@ for _ in range(0, 10):
 
   # Verificando se o arquivo "taxa-cdi.csv" existe
 
-  if os.path.exists('./01_python_exercises/09_scripting/taxa-cdi.csv') == False:
+  if os.path.exists('./taxa-cdi.csv') == False:
 
-    with open(file='./01_python_exercises/09_scripting/taxa-cdi.csv', mode='w', encoding='utf8') as fp:
+    with open(file='./taxa-cdi.csv', mode='w', encoding='utf8') as fp:
       fp.write('data,hora,taxa\n')
 
   # Salvando dados no arquivo "taxa-cdi.csv"
 
-  with open(file='./01_python_exercises/09_scripting/taxa-cdi.csv', mode='a', encoding='utf8') as fp:
+  with open(file='./taxa-cdi.csv', mode='a', encoding='utf8') as fp:
     fp.write(f'{data},{hora},{cdi}\n')
 
   time.sleep(2 + (random() - 0.5))
